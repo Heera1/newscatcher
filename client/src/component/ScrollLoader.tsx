6688d156e9835e5ca0fdfc96dfc14a25e8c7af7f
JiEun An <99064439+Heera1@users.noreply.github.com>
2023-05-31 15:04:36 +0900

    Revert "1차 제출 PR"

--- client/src/component/ScrollLoader.tsx
+++
@@ -1,8 +0,0 @@
-import ReactLoading from "react-loading";
-export default function ScrollLoader() {
-  return (
-    <div className="flex items-center justify-center w-full mb-10 text-center h-3/4">
-      <ReactLoading type="spin" color="black"></ReactLoading>
-    </div>
-  );
-}
